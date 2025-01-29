3fd05d67f944e71778af0641779354275d0fcefe
vidulpramitha <vidulpramitha@gmail.com>
2025-01-30 04:49:19 +0530

    test 1

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified', 'prevent-back-button'])->name('dashboard');
 
 Route::middleware(['auth', 'prevent-back-button'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/studentRegister', function () {
             return view('studentRegister');
